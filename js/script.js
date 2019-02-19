9796df25718ce8f53fe993c47b3d76248e72d26c
Dominik Rodzim <dominik.rodzim95@gmail.com>
2019-02-19 22:26:41 +0100

    Add slider

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  hash: true,
+});
+
+var reset = document.querySelector('#reset');
+
+reset.addEventListener( 'click', function() {
+  flkty.select( 0 );
+})
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
