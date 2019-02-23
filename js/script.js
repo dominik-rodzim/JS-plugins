47dd2dda61894a90156462126d46273a18ee6c3c
Dominik Rodzim <dominik.rodzim95@gmail.com>
2019-02-24 00:53:35 +0100

    Tasks 3-4 finished

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ var reset = document.querySelector('#reset');
 
 reset.addEventListener( 'click', function() {
   flkty.select( 0 );
-})
+});
 
 var progressBar = document.querySelector('.progress-bar')
 
@@ -19,5 +19,29 @@ flkty.on( 'scroll', function( progress ) {
   progressBar.style.width = progress * 100 + '%';
 });
 
-// task2
+// map
+
+window.initMap = function() {
+  var map = new google.maps.Map(
+      document.getElementById('map'), {
+      	zoom: 15, 
+      	center: sliderData[0].coords
+      });
+  for(let i = 0; i < sliderData.length; i++){
+  	var marker = new google.maps.Marker({
+  		position: sliderData[i].coords, 
+  		map: map
+  	});
+  	marker.addListener( 'click', function() {
+	  flkty.select( i );
+	})
+  } 
+	flkty.on( 'change', function( index ) {
+		var center = new google.maps.LatLng(sliderData[index].coords);
+		map.panTo(center);
+	});
+}
+
+
+
 
