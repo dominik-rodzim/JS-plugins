66a044d10e96e93d6007cafcd88118e70ee6c8e9
Dominik Rodzim <dominik.rodzim95@gmail.com>
2019-02-26 18:10:21 +0100

    Adjust JS file structure

--- js/script.js
+++ js/script.js
@@ -9,33 +9,32 @@ var flkty = new Flickity( elem, {
 var reset = document.querySelector('#reset');
 
 reset.addEventListener( 'click', function() {
-  flkty.select( 0 );
+	flkty.select( 0 );
 });
 
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
+	progress = Math.max( 0, Math.min( 1, progress ) );
+	progressBar.style.width = progress * 100 + '%';
 });
 
 // map
 
 window.initMap = function() {
-  var map = new google.maps.Map(
-      document.getElementById('map'), {
-      	zoom: 15, 
-      	center: sliderData[0].coords
-      });
-  for(let i = 0; i < sliderData.length; i++){
-  	var marker = new google.maps.Marker({
-  		position: sliderData[i].coords, 
-  		map: map
-  	});
-  	marker.addListener( 'click', function() {
-	  flkty.select( i );
-	})
-  } 
+	var map = new google.maps.Map(document.getElementById('map'), {
+    	zoom: 15, 
+  		center: sliderData[0].coords
+	});
+	for(let i = 0; i < sliderData.length; i++){
+	  	var marker = new google.maps.Marker({
+	  		position: sliderData[i].coords, 
+	  		map: map
+  		});
+	  	marker.addListener( 'click', function() {
+			flkty.select( i );
+		});
+	} 
 	flkty.on( 'change', function( index ) {
 		var center = new google.maps.LatLng(sliderData[index].coords);
 		map.panTo(center);
